7675ffd9a46333df1139ff1f01931a9da73b7589
Celo Reis <hello@celoreis.dev>
2021-09-26 19:45:09 -0300

    fix: better responsive background

--- lib/home-animation.ts
+++ lib/home-animation.ts
@@ -68,6 +68,7 @@ function onResize(
   renderer: THREE.WebGLRenderer
 ) {
   camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
