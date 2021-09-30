ae2808f4ebdcde5a3698fc49479bd9416d02fa47
Celo Reis <hello@celoreis.dev>
2021-09-30 08:10:15 -0300

    feat: update meta info

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,43 @@ export default class MyDocument extends Document {
             href="https://fonts.gstatic.com"
             crossOrigin="true"
           />
+          <meta name="title" content="Marcelo Reis - Frontend Developer" />
+          <meta
+            name="description"
+            content="Frontend developer, passionate about Javascript, Typescript and React. Living in Belo Horizonte and trying to develop some awesome stuff"
+          />
+
+          {/* Open Graph / Facebook */}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://celoreis.dev/" />
+          <meta
+            property="og:title"
+            content="Marcelo Reis - Frontend Developer"
+          />
+          <meta
+            property="og:description"
+            content="Frontend developer, passionate about Javascript, Typescript and React. Living in Belo Horizonte and trying to develop some awesome stuff"
+          />
+          <meta
+            property="og:image"
+            content="https://celoreis.dev/img/meta-image.png"
+          />
+
+          {/* Twitter */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://celoreis.dev/" />
+          <meta
+            property="twitter:title"
+            content="Marcelo Reis - Frontend Developer"
+          />
+          <meta
+            property="twitter:description"
+            content="Frontend developer, passionate about Javascript, Typescript and React. Living in Belo Horizonte and trying to develop some awesome stuff"
+          />
+          <meta
+            property="twitter:image"
+            content="https://celoreis.dev/img/meta-image.png"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Kalam:wght@300;400;700&display=swap"
             rel="stylesheet"
