1683bbde976225999208a49633abddd828a39444
Celo Reis <marcelo@tryalma.ai>
2024-10-09 12:02:07 -0300

    chore: Reboot project

--- next-sitemap.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://celoreis.dev",
-  generateRobotsTxt: true,
-};
--- src/libs/useLocale.tsx
+++
@@ -1,6 +0,0 @@
-import { useRouter } from "next/router";
-
-export function useLocale() {
-  const router = useRouter();
-  return router.locale ?? "en";
-}
