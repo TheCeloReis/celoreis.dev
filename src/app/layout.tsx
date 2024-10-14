e14db7bbb3b8c8b77357033108763d0abe304822
Celo Reis <marcelo@tryalma.ai>
2024-10-13 21:20:24 -0300

    feat: Adds analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "@/styles/globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return children;
+  return (
+    <>
+      <GoogleAnalytics gaId="G-8C4BT53JDY" />
+      {children}
+    </>
+  );
 }
