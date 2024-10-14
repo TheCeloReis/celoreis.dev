a03951780efc1a3c1110c0df416d79c111038485
Celo Reis <marcelo@tryalma.ai>
2024-10-13 21:32:56 -0300

    fix: Display analytics only on prod

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,9 @@ export default function RootLayout({
 }) {
   return (
     <>
-      <GoogleAnalytics gaId="G-8C4BT53JDY" />
+      {process.env.NODE_ENV === "production" && (
+        <GoogleAnalytics gaId="G-8C4BT53JDY" />
+      )}
       {children}
     </>
   );
