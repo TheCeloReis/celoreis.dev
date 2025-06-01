7656a4466cc4995f478d59b4f56ef61d45b86389
Celo Reis <marcelo@tryalma.ai>
2025-06-01 12:30:32 -0300

    feat: Customize chat

--- src/components/ChatWidget.tsx
+++ src/components/ChatWidget.tsx
@@ -8,7 +8,16 @@ const ChatWidgetComponent = dynamic(
 );
 
 const ChatWidget = () => (
-  <ChatWidgetComponent genAIAPIKey={process.env.NEXT_PUBLIC_GEN_AI_API_KEY} />
+  <ChatWidgetComponent
+    genAIAPIKey={process.env.NEXT_PUBLIC_GEN_AI_API_KEY}
+    theme={{
+      primaryColor: "#1d6681",
+    }}
+    header={{
+      title: "Celo Reis (AI agent)",
+      description: "Ask me anything!",
+    }}
+  />
 );
 
 export default ChatWidget;
